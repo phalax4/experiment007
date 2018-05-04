38851cf16e816ef141a69fc00129b6933d306129
phalax4 <jeremylim995@gmail.com>
2018-05-03 19:46:22 -0700

    Moved data to testing code

--- src/main/java/com/experiment/Application.java
+++ src/main/java/com/experiment/Application.java
@@ -1,37 +1,14 @@
 package com.experiment;
 
-import java.util.*;
-
 public class Application {
 
     public static void main(String[]args){
         System.out.println("Starting application");
 
-        Person m0 = new Person("m0", new ArrayList(Arrays.asList("w1", "w0", "w2")));
-        Person m1 = new Person("m1", new ArrayList(Arrays.asList("w2", "w1", "w0")));
-        Person m2 = new Person("m2", new ArrayList(Arrays.asList("w0", "w2", "w1")));
-
-        Person w0 = new Person("w0", new ArrayList(Arrays.asList("m1", "m0", "m2")));
-        Person w1 = new Person("w1", new ArrayList(Arrays.asList("m2", "m1", "m0")));
-        Person w2 = new Person("w2", new ArrayList(Arrays.asList("m0", "m2", "m1")));
-
-
-        Person p0 = new Person("p1", new ArrayList(Arrays.asList("a2", "a1", "a3", "a4")));
-        Person p1 = new Person("p2", new ArrayList(Arrays.asList("a4", "a1", "a2", "a3")));
-        Person p2 = new Person("p3", new ArrayList(Arrays.asList("a1", "a3", "a2","a4")));
-        Person p3 = new Person("p4", new ArrayList(Arrays.asList("a2", "a3", "a1", "a4")));
-
-
-        Person a0 = new Person("a1", new ArrayList(Arrays.asList("p1", "p3", "p2", "p4")));
-        Person a1 = new Person("a2", new ArrayList(Arrays.asList("p3", "p4", "p1", "p2")));
-        Person a2 = new Person("a3", new ArrayList(Arrays.asList("p4", "p2", "p3","p1")));
-        Person a3 = new Person("a4", new ArrayList(Arrays.asList("p3", "p2", "p1", "p4")));
-
-
 
         Matching m = new Matching();
-        m.matchPeopleBasic(new ArrayList<Person>(Arrays.asList(m0, m1, m2)),new ArrayList<Person>(Arrays.asList(w0, w1, w2)));
-        m.matchPeopleBasic(new ArrayList<Person>(Arrays.asList(p0, p1, p2, p3)), new ArrayList<Person>(Arrays.asList(a0, a1, a2,a3)));
+
+
     }
 
 
--- src/main/java/com/experiment/Matching.java
+++ src/main/java/com/experiment/Matching.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Matching {
     //p1-1, p2 - 4, p3 =3, p4 -2
     //m0 - 1, m1-2, m2-0
-    public void matchPeopleBasic(ArrayList<Person> proposers, ArrayList<Person> acceptors) {
+    public HashMap<String, String> matchPeopleBasic(ArrayList<Person> proposers, ArrayList<Person> acceptors) {
 
         HashMap<String, Person> wHashMap = new HashMap();
         HashMap<String, Person> mHashMap = new HashMap();
@@ -42,18 +42,29 @@ public class Matching {
             }
         }
 
-        System.out.println("Results...");
+        System.out.println("Results:");
+
+        HashMap<String, String> resultMap = new HashMap();
 
         for (Map.Entry<String, Person> entry : mHashMap.entrySet()) {
             System.out.println(entry.getKey() + "...." + entry.getValue().getEngagedTo());
+            resultMap.put(entry.getKey(), entry.getValue().getEngagedTo());
         }
+
         for (Map.Entry<String, Person> entry : wHashMap.entrySet()) {
             System.out.println(entry.getKey() + "...." + entry.getValue().getEngagedTo());
         }
+
+        return resultMap;
+
     }
 
-
-    public void matchPeopleHospitalResident(ArrayList<Person> proposers, ArrayList<Department> acceptors) {
+    /**
+     * Favor proposers or Residents
+     * @param proposers
+     * @param acceptors
+     */
+    public HashMap<String, String> matchResidentToHospital(ArrayList<Person> proposers, ArrayList<Department> acceptors) {
         HashMap<String, Person> pHashMap = new HashMap();
         HashMap<String, Department> aHashMap = new HashMap();
 
@@ -62,15 +73,19 @@ public class Matching {
         for(Department d: acceptors){
             aHashMap.put(d.getId(), d);
         }
+        System.out.println("Number of students: " + proposers.size());
 
         for(Person p : proposers){
             q.add(p);
             pHashMap.put(p.getId(), p);
         }
 
+        System.out.println(q.size());
+
         while(!q.isEmpty()){
             Person proposer = q.poll();
-            if(proposer.unproposedIdx == proposer.getPref().size()){
+            System.out.println("proposer: " + proposer.getId());
+            if(proposer.unproposedIdx > proposer.getPref().size()){
                 System.out.println("Out of proposals for this person");
                 continue;
             }
@@ -78,16 +93,42 @@ public class Matching {
             proposer.unproposedIdx++;
                if(currDept.getRemainingSlots() > 0 ){
                     currDept.addToWaitList(proposer.getId());
+                    //System.out.println("Adding to " + currDept.getId()+ " with " + proposer.getId());
                     proposer.setEngagedTo(currDept.getId());
-               }else if(currDept.prefers(proposer)){ //Dept has filled up slots but prefers this higher ranked proposer
-                    String removedId = currDept.removeAndReplaceLeastPrefered(proposer.getId());
-                    proposer.setEngagedTo(currDept.getId());
+               }else {
+                   if(currDept.prefers(proposer)) { //Dept has filled up slots but prefers this higher ranked proposer
+                       String removedId = currDept.removeAndReplaceLeastPrefered(proposer.getId());
+                       proposer.setEngagedTo(currDept.getId());
+                       //System.out.println("Adding2 to " + currDept.getId() + " with " + proposer.getId());
 
-                    pHashMap.get(removedId).setEngagedTo(null);
-                    q.add(pHashMap.get(removedId));
+                       pHashMap.get(removedId).setEngagedTo(null);
+                       q.add(pHashMap.get(removedId));
+                       //System.out.println("Student added back on q: " + pHashMap.get(removedId));
+                   }else{
+                        q.add(proposer);
+                   }
                }
 
         }
+
+        HashMap<String, String> resultMap = new HashMap();
+
+        for (Map.Entry<String, Person> entry : pHashMap.entrySet()) {
+            System.out.println(entry.getKey() + "...." + entry.getValue().getEngagedTo());
+            resultMap.put(entry.getKey(), entry.getValue().getEngagedTo());
+        }
+
+        //for (Map.Entry<String, Department> entry : aHashMap.entrySet()) {
+        //    System.out.println(entry.getKey() + "...." + entry.getValue().getEngagedTo());
+        //}
+
+        return resultMap;
+    }
+
+    /**
+     * Favor Hospitals or acceptors
+     */
+    public void matchHospitalToResidents(){
 
     }
 }
